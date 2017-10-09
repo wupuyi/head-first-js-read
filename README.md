7b417c5eb8bea5be904a6fe6e2f829514166c2f7
wupuyi <wupuyi@foxmail.com>
2017-10-09 20:12:53 +0800

    commit

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 ### [2.0 版本](zhanjianGAME/2.0)
 
 2.0 版本试玩地址: [https://wupuyi.github.io/head-first-js-read/zhanjianGAME/2.0/](https://wupuyi.github.io/head-first-js-read/zhanjianGAME/2.0/)
+
+## 2. 测试猜图小游戏
+
+地址：[点我！！！点我！！！点我！！！]()
--- testGame/js/index.js
+++ testGame/js/index.js
@@ -1,1 +1,19 @@
-var img
+window.onload = init;
+
+function init() {
+    // var image = document.getElementById("one");
+    // image.onclick = showAnswer;
+    
+    // 给所有img元素绑定showAswer事件
+    var image = document.getElementsByTagName("img");
+    for (var i = 0; i < image.length; i++) {
+        images[i].onclick = showAnswer;
+    }
+}
+
+function showAnswer() {
+    // var image = document.getElementById("one");
+    // image.src = "img/001.jpg";
+
+}
+
