ec0fbd2086b4e8bff033deb9af0e0a1d62bae32d
wupuyi <wupuyi@foxmail.com>
2017-10-13 20:20:24 +0800

    commit

--- javscript-dom/js/index.js
+++ javscript-dom/js/index.js
@@ -1,10 +1,25 @@
+
+// 点击链接切换图片
 function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
-    var text = whichpic.getAttribute("title");
+    if(whichpic.getAttribute("title")) {
+        var text = whichpic.getAttribute("title");
+    } else {
+        var text = "";
+    }
+
+    // var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "" ;
+    
     var placeholder = document.getElementById("placeholder");
     var description = document.getElementById("description");
+    if(!placeholder) return false;
     placeholder.setAttribute("src",source);
-    description.firstChild.nodeValue = text;
+    if(description){
+        if(description.firstChild.nodeType == 3){
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
 }
 
 // function countBodyChildren() {
@@ -14,3 +29,36 @@ function showPic(whichpic) {
 // }
 
 // window.onload = countBodyChildren;
+
+
+
+// 添加事件处理函数。
+function prepareGallery() {
+    var imageGallery = document.getElementById("imagegallery");
+    // 判断对DOM的支持。
+    if (!document.getElementById || !document.getElementsByTagName) return false; 
+    if(!imageGallery) return false;
+    var links = imageGallery.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++) {
+        links[i].onclick = function() {
+            //返回执行结果
+            return !showPic(this);
+        }
+    }
+}
+
+// window.onload = prepareGallery;
+
+function addLoadEvent(func) {
+    var oldOnload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldOnload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
