78cc3ed706b2cf703d82456e4f0f5978bcad7ac0
wupuyi <wupuyi@foxmail.com>
2017-10-20 13:45:35 +0800

    commit

--- learn-js/chapter5/demo1.js
+++ learn-js/chapter5/demo1.js
@@ -16,7 +16,45 @@ function factorial2(num) {
     }
 }
 
-
 console.log(factorial(6));
 
 console.log(factorial2(6));
+
+
+window.color = "red";
+var o ={
+    color: "blue"
+};
+function sayColor() {
+    console.log(this.color);
+}
+
+sayColor();
+
+sayColor.call(this);
+
+sayColor.call(window);
+
+sayColor.call(o);
+
+sayColor.apply(o);
+
+var objectSayColor = sayColor.bind(o);
+
+objectSayColor();
+
+var s1 = "some text";
+var s2 = s1.substring(2);
+
+console.log(s1.indexOf("s"));
+
+// alert(s2);
+
+
+var stringValue = "   hello world   ";
+
+// console.log(stringValue.indexOf("o",6));
+
+var trimmedStringValue = stringValue.trim();
+
+console.log(trimmedStringValue);
