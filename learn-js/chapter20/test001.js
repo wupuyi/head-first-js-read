8e2ace5ad52ceb1aad140788b6ef55c7f65d0385
wupuyi <wupuyi@foxmail.com>
2017-11-05 22:04:42 +0800

    commit

---
+++ learn-js/chapter20/test001.js
@@ -0,0 +1,10 @@
+var book = {
+    title: "Professional JavaScript",
+    authors: [
+        "Nicholas C. Zakas"
+    ],
+    edition: 3,
+    year: 2011
+};
+
+var jsonText = JSON.stringify(book);
